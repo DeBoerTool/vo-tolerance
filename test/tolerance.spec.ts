a674b4f3307be157388200f2c98cf1db140d2d4e
Dan Deboer <daniel.s.deboer@gmail.com>
2019-01-31 09:15:07 -0500

    Test cleanup

--- test/tolerance.spec.ts
+++ test/tolerance.spec.ts
@@ -1,12 +1,13 @@
 import Precision from '@deboertool/vo-precision'
 import { Factory } from '@deboertool/vo-unit'
 import { expect } from 'chai'
-import Tolerance from '../src/Tolerance'
+import { Tolerance } from '../'
 
 const positiveValue = 99.99
 const negativeValue = -84
-const positiveVo = new Tolerance(positiveValue, new Precision(2), new Factory().make('in'))
-const negativeVo = new Tolerance(negativeValue, new Precision(2), new Factory().make('mm'))
+const units = new Factory()
+const positiveVo = new Tolerance(positiveValue, new Precision(2), units.make('in'))
+const negativeVo = new Tolerance(negativeValue, new Precision(2), units.make('mm'))
 
 describe('Tolerance', () => {
   it(
@@ -14,29 +15,29 @@ describe('Tolerance', () => {
     () => expect(positiveVo.value()).to.equal(positiveValue)
   )
   
-    it(
-      'returns a plus for positive values',
-      () => expect(positiveVo.sign()).to.equal('+')
-    )
-  
-    it(
-      'returns no sign for positive values since negative values are already signed',
-      () => expect(negativeVo.sign()).to.equal('')
-    )
-  
-    it(
-      'returns the signed value as a string',
-      () => {
-        expect(negativeVo.signed()).to.equal('-84')
-        expect(positiveVo.signed()).to.equal('+99.99')
-      }
-    )
+  it(
+    'returns a plus for positive values',
+    () => expect(positiveVo.sign()).to.equal('+')
+  )
 
-    it(
-      'returns a fully formatted string with units',
-      () => {
-        expect(positiveVo.toString()).to.equal('+99.99"')
-        expect(positiveVo.toString()).to.equal('-84mm')
-      }
-    )
+  it(
+    'returns no sign for positive values since negative values are already signed',
+    () => expect(negativeVo.sign()).to.equal('')
+  )
+
+  it(
+    'returns the signed value as a string',
+    () => {
+      expect(positiveVo.signed()).to.equal('+99.99')
+      expect(negativeVo.signed()).to.equal('-84')
+    }
+  )
+
+  it(
+    'returns a fully formatted string with units',
+    () => {
+      expect(positiveVo.toString()).to.equal('+99.99"')
+      expect(negativeVo.toString()).to.equal('-84mm')
+    }
+  )
 })
